77d8235666e59f38bc31b53f04a270617511eb28
LomakinAlex <143326019+LomakinAlex@users.noreply.github.com>
2024-01-27 13:42:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,9 @@
-# Here's a Python implementation of Zeller's Congruence
-def get(day, month, year):
+def dayname(day:int, month:int, year:int) -> str:
     if month < 3:
         month += 12
         year -= 1
-
-    K = year % 100
-    J = year // 100
-
-    h = (day + (13 * (month + 1)) // 5 + K + K // 4 + J // 4 - 2 * J) % 7
-    d = (h + 5) % 7
-    match d:
-        case 0: return "Saturday"
-        case 1: return"Sunday"
-        case 2: return "Monday"
-        case 3: return "Tuesday"
-        case 4: return "Wednesday"
-        case 5: return "Thursday"
-        case 6: return "Friday"
+    K:int = year % 100
+    J:int = year // 100
+    h:int = (day + (13 * (month + 1)) // 5 + K + K // 4 + J // 4 - 2 * J) % 7
+    d:int = (h + 5) % 7
+    return ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")[d]
